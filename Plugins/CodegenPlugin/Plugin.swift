748a24d8c4613f6f6c3ce5f583f713bb9fea1710
Luke Lau <luke_lau@icloud.com>
2022-07-11 11:47:18 +0100

    Add XcodeBuildToolPlugin conformance

--- Plugins/CodegenPlugin/Plugin.swift
+++ Plugins/CodegenPlugin/Plugin.swift
@@ -2,28 +2,44 @@ import Foundation
 import PackagePlugin
 import XcodeProjectPlugin
 
-@main struct CodegenPlugin: XcodeBuildToolPlugin {
-    func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+
+@main struct CodegenPlugin: BuildToolPlugin {
+    func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
         let tool = try context.tool(named: "swiftui-graphql-codegen")
         guard let target = target as? SourceModuleTarget else {
             return []
         }
-        let schema = target.directory.appending(subpath: "schema.json")
-        let graphqlCommands = target.sourceFiles(withSuffix: "graphql").map { inputFile in
-            let outputPath = context.pluginWorkDirectory.appending(inputFile.path.stem + ".swift")
+        return createCommands(tool: tool,
+                              schemaPath: target.directory.appending(subpath: "schema.json"),
+                              pluginWorkDirectory: context.pluginWorkDirectory,
+                              graphqlFiles: target.sourceFiles(withSuffix: "graphql"))
+    }
+
+    func createCommands(tool: PluginContext.Tool, schemaPath: Path, pluginWorkDirectory: Path, graphqlFiles: some Sequence<File>) -> [Command] {
+        let graphqlCommands = graphqlFiles.map { inputFile in
+            let outputPath = pluginWorkDirectory.appending(inputFile.path.stem + ".swift")
             return Command.buildCommand(
                 displayName: "Generate GraphQL types for \(inputFile.path.lastComponent)",
                 executable: tool.path,
-                arguments: ["--output", outputPath.string, "--schema", schema.string, inputFile.path.string],
-                inputFiles: [inputFile.path, schema],
+                arguments: ["--output", outputPath.string, "--schema", schemaPath.string, inputFile.path.string],
+                inputFiles: [inputFile.path, schemaPath],
                 outputFiles: [outputPath])
         }
-        let schemaOutputPath = context.pluginWorkDirectory.appending("schema.swift")
-        let schemaTypesCommand = Command.buildCommand(displayName: "Generate GraphQL enum types for \(schema)",
+        let schemaOutputPath = pluginWorkDirectory.appending("schema.swift")
+        let schemaTypesCommand = Command.buildCommand(displayName: "Generate GraphQL enum types for \(schemaPath)",
                                                       executable: tool.path,
-                                                      arguments: ["--output", schemaOutputPath, "--schema", schema.string],
-                                                      inputFiles: [schema],
+                                                      arguments: ["--output", schemaOutputPath, "--schema", schemaPath.string],
+                                                      inputFiles: [schemaPath],
                                                       outputFiles: [schemaOutputPath])
         return [schemaTypesCommand] + graphqlCommands
+    }
+}
+
+extension CodegenPlugin: XcodeBuildToolPlugin {
+    func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+        createCommands(tool: try context.tool(named: "swiftui-graphql-codegen"),
+                       schemaPath: context.xcodeProject.directory.appending(subpath: "schema.json"),
+                       pluginWorkDirectory: context.pluginWorkDirectory,
+                       graphqlFiles: target.inputFiles.filter { $0.type == .source && $0.path.extension == "graphql" })
     }
 }
